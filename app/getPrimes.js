c4ddfa8d194428219df692741e110ee794a0a25c
WillyWunderdog <odemichaelgbenga@gmail.com>
2017-07-03 20:43:29 +0100

    clean up getPrimes function

--- app/getPrimes.js
+++ app/getPrimes.js
@@ -5,24 +5,29 @@ module.exports = {
  getPrimes:(range) => {
    let primes = [];
 
-    if(typeof range === "string"){
-      return 'please enter a number'
-    }else if(range === 0 || range <= 1){
-      return 'input a value above 1'
-    }
-    else{
-      const isPrime = (num) => {
-    if(num => 2){
+   const isPrime = (num) => {
+    if(num => 2) {
       for(let counter = 2; counter < num; counter++){
-         if(num % counter === 0){
+         if(num % counter === 0) {
             return false;
-         }
-       }
-      return true;
-     }
+          }
+        }
+        return true;
+      }
       return false;
-     }
-     for(let counter = 2; counter <= range; counter++){
+    }
+
+    if(typeof range === "string") {
+      return 'please enter a number';
+    }
+
+    else if(range <= 1) {
+      return 'input a value above 1';
+    }
+
+    else{
+      for(let counter = 2; counter <= range; counter++){
+         
          if(isPrime(counter)){
             primes.push(counter);
          }
@@ -30,5 +35,4 @@ module.exports = {
       return primes;
     }
   }
-
 }
